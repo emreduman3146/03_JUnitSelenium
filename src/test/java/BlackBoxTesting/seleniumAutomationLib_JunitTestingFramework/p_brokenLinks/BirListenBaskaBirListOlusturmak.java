202f67d1a41376eb7a0471ddd4550153ff20af71
emreduman3000 <emryduman@gmail.com>
2023-01-27 20:06:16 -0600

    Finding broken links via Selenium by using java functional programming

--- src/test/java/BlackBoxTesting/seleniumAutomationLib_JunitTestingFramework/p_brokenLinks/BirListenBaskaBirListOlusturmak.java
+++ src/test/java/BlackBoxTesting/seleniumAutomationLib_JunitTestingFramework/p_brokenLinks/BirListenBaskaBirListOlusturmak.java
@@ -4,32 +4,48 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 
 public class BirListenBaskaBirListOlusturmak
 {
 
     public static void main(String[] args) {
-        javaStructuralProgramming();
+      //  javaStructuralProgramming();
+        javaFunctionalProgrammingIle();
     }
 
-    public static void javaStructuralProgramming() {
+
+    public static void javaStructuralProgramming()
+    {
+
+
         List<Integer> originalList = new ArrayList<>();
+
+        //NORMAL FOR LOOP
         for (int i = 1; i <= 5; i++) {
             originalList.add(i);
         }
+
+        //FOR EACH LOOP
         for (Integer i : originalList) {
             System.out.println(i);
         }
 
+        //yeni bir liste olusturmak
         List<Integer> newList = new ArrayList<>();
-        for (Integer i : originalList) {
+        for (Integer i : originalList)
+        {
             newList.add(i * i);
         }
+
         for (Integer i : newList) {
             System.out.println(i);
         }
 
+
         List<Integer> newList1 = new ArrayList<>();
+
         for (Integer i : originalList) {
             if (i % 2 == 0) {
                 newList1.add(i);
@@ -38,24 +54,33 @@ public class BirListenBaskaBirListOlusturmak
         for (Integer i : newList1) {
             System.out.println(i);
         }
+
     }
 
-    public static void javaFunctionalProgramlamaIle()
+    private static void javaFunctionalProgrammingIle()
     {
-        List<Integer> originalList = IntStream.range(1, 6)
-                .boxed()
-                .collect(Collectors.toList());
+        //JAVA STREAM KONUSU
+        //Stream ifadesini List'e donusturelim
+       List<Integer> integerList= IntStream.range(1,6).boxed().collect(Collectors.toList());
+        integerList.forEach(element->System.out.println(element));
 
-        originalList.forEach(System.out::println);
 
-
-        List<Integer> newList = originalList.stream().map(i -> i * i).collect(Collectors.toList());
+        //Elimizdeki varolan bir listten yeni bir liste olusturmak
+        List<Integer> newList=integerList.stream().map(i->i*i).collect(Collectors.toList());
         newList.forEach(System.out::println);
 
 
-        List<Integer> newList1 = originalList.stream().filter(i -> i%2 == 0).collect(Collectors.toList());
-        newList1.forEach(System.out::println);
 
+        //Elimizdeki varolan bir listten yeni bir liste olusturmak
+        List<Integer> newList2=integerList.stream().filter(i->i%2==0).collect(Collectors.toList());
+        newList2.forEach(System.out::println);
 
     }
+
+
+
+    //FunctionalProgramlamayi kullanirlar, String hazir classini kullanmak
+    //Functional programlama yazma, Kendi Functional classimi kullanmak
+
+
 }
